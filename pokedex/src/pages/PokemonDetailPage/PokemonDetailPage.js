5b3c80b5ae2011951098f40fef52b61c3ae3f7c1
Igor Matsuoka <igor.mtka@gmail.com>
2022-01-03 18:15:22 -0300

    rotas e postman

---
+++ pokedex/src/pages/PokemonDetailPage/PokemonDetailPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PokemonDetailPage = () => {
+    return(
+        <div>
+            <h1>Detalhes</h1>
+        </div>
+    )
+}
+
+export default PokemonDetailPage;
