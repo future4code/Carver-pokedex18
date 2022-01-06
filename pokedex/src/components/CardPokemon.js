038b7b424645014d41007e471ba8fcf03b686b42
VICTOR FERREIRA DO VALE <victor.vale@uvvnet.com.br>
2022-01-05 22:19:47 -0300

    pokedex

--- pokedex/src/components/CardPokemon.js
+++ pokedex/src/components/CardPokemon.js
@@ -1,14 +1,23 @@
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import PokeImagem from "./ImagemPoke";
+import {goToDetail} from "../routes/Coordinator"
 /* import GlobalContext from "../../global/GlobalContext.js"; */
 
 function CardPokemon(props) {
+  const history = useHistory()
   return (
     <div>
       <PokeImagem name={props.pokename} />
+  
+      <p>{props.name}</p>
+      <p>{props.id}</p>
+     
       <div>
         <button>Adicionar</button>
-        <button>Detalhes</button>
+        <button onClick={() => goToDetail(history, props.id)}>Detalhes</button>
+        
+       
       </div>
     </div>
   );
